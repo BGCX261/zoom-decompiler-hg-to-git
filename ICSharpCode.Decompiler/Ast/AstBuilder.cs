dnSpy/dnSpy

commit 51fc2f10cc1cd224ea2c9d6f5b692e8ebaf2c956

Look for generic type parameters defined in TypeDefinition, allows us to display angle brackets in unbound type names, e.g., typeof(List<>) fixes #179

ICSharpCode.Decompiler/Ast/AstBuilder.cs
@@ -436,6 +436,15 @@ namespace ICSharpCode.Decompiler.Ast
 						astType = new MemberType { Target = nsType, MemberName = name };
 					} else {
 						astType = new SimpleType(name);
+						
+						// Look for generic type parameters defined in TypeDefinition
+						// allows us to display angle brackets in unbound type names
+						// e.g. typeof(List<>)
+						TypeDefinition resolvedType = type.Resolve();
+
+						for (int i = 0; i < resolvedType.GenericParameters.Count; i++) {
+							((SimpleType)astType).TypeArguments.Add(new SimpleType(""));
+						}
 					}
 					astType.AddAnnotation(type);
 					
@@ -1433,7 +1442,7 @@ namespace ICSharpCode.Decompiler.Ast
 					if (baseType.HasFields && AnyIsHiddenBy(baseType.Fields, member))
 						return true;
 					if (includeBaseMethods && baseType.HasMethods
-							&& AnyIsHiddenBy(baseType.Methods, member, m => !m.IsSpecialName))
+					    && AnyIsHiddenBy(baseType.Methods, member, m => !m.IsSpecialName))
 						return true;
 					if (baseType.HasNestedTypes && AnyIsHiddenBy(baseType.NestedTypes, member))
 						return true;
@@ -1447,8 +1456,8 @@ namespace ICSharpCode.Decompiler.Ast
 			where T : IMemberDefinition
 		{
 			return members.Any(m => m.Name == derived.Name
-				&& (condition == null || condition(m))
-				&& TypesHierarchyHelpers.IsVisibleFromDerived(m, derived.DeclaringType));
+			                   && (condition == null || condition(m))
+			                   && TypesHierarchyHelpers.IsVisibleFromDerived(m, derived.DeclaringType));
 		}
 		
 		/// <summary>

BGCX261/zoom-decompiler-hg-to-git

commit 23a3c217e41de8887b94aeef7ec5ef213a3a171b

fix bug when printing type names introduced by commit 51fc2f10cc1cd224ea2c9d6f5b692e8ebaf2c956

ICSharpCode.Decompiler/Ast/AstBuilder.cs
@@ -437,13 +437,15 @@ namespace ICSharpCode.Decompiler.Ast
 					} else {
 						astType = new SimpleType(name);
 						
-						// Look for generic type parameters defined in TypeDefinition
-						// allows us to display angle brackets in unbound type names
-						// e.g. typeof(List<>)
-						TypeDefinition resolvedType = type.Resolve();
-
-						for (int i = 0; i < resolvedType.GenericParameters.Count; i++) {
-							((SimpleType)astType).TypeArguments.Add(new SimpleType(""));
+						if (!type.HasGenericParameters) {
+							// Look for generic type parameters defined in TypeDefinition
+							// allows us to display angle brackets in unbound type names
+							// e.g. typeof(List<>)
+							TypeDefinition resolvedType = type.Resolve();
+	
+							for (int i = 0; i < resolvedType.GenericParameters.Count; i++) {
+								((SimpleType)astType).TypeArguments.Add(new SimpleType(""));
+							}
 						}
 					}
 					astType.AddAnnotation(type);